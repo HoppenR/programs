ff7d8aa467d2456fd3a6cc670c07876d59f6a3ee
Christoffer Lundell <christofferlundell@protonmail.com>
2020-01-11 17:33:55 +0100

    Make solution #4 work with more digits

--- cpp/ProjectEuler/4/main.cpp
+++ cpp/ProjectEuler/4/main.cpp
@@ -2,20 +2,20 @@
 #include <math.h>
 #include <vector>
 
-std::vector<int> get_digits(long number) {
+std::vector<int> get_digits(const long number) {
 	std::vector<int> Digits;
 	for (int i = log10(number); i >= 0; i--) {
-		Digits.push_back(number / static_cast<int>(pow(10, i)) % 10);
+		Digits.push_back(number / static_cast<long>(pow(10, i)) % 10);
 	}
 	return Digits;
 }
 
-long find_largest_palindromic_number(int numdigits) {
-	std::vector<int> Digits;
+long find_largest_palindromic_number(const int numdigits) {
 	long largestPalindrome = 0;
-	for (long i = pow(10, numdigits - 1) * 9; i < pow(10, numdigits); i++) {
-		for (long j = pow(10, numdigits - 1) * 9; j < pow(10, numdigits); j++) {
-			long product = i * j;
+	const long target = pow(10, numdigits);
+	for (long i = pow(10, numdigits - 1) * 9; i < target; i++) {
+		for (long j = pow(10, numdigits - 1) * 9; j < target; j++) {
+			const long product = i * j;
 			// Check if we can skip iteration
 			if (product < largestPalindrome)
 				continue;
@@ -23,16 +23,16 @@ long find_largest_palindromic_number(int numdigits) {
 			// divisible by 11.
 			if (product % 11 != 0)
 				continue;
-			Digits = get_digits(product);
+			std::vector<int> Digits = get_digits(product);
 			bool valid = true;
-			for (size_t ii = 0; ii < Digits.size(); ii++) {
+			for (size_t ii = 0; ii < Digits.size() / 2; ii++) {
 				if (Digits.at(ii) != Digits.at(Digits.size() - ii - 1)) {
 					valid = false;
 					break;
 				}
 			}
 			if (valid) {
-				largestPalindrome = std::max(largestPalindrome, product);
+				largestPalindrome = product;
 			}
 		}
 	}
