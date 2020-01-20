26733ec27a1a42331c4260d0d60733f616b96e0f
Christoffer Lundell <christofferlundell@protonmail.com>
2020-01-20 20:25:42 +0100

    Clean up range summation

--- cpp/ProjectEuler/6/main.cpp
+++ cpp/ProjectEuler/6/main.cpp
@@ -3,10 +3,8 @@
 
 long sum_square_difference(long num) {
 	num = std::abs(num);
-	// My attempt at summing every number from 1 to num,
-	// the right hand side of the plus-sign is for when num is odd.
-	long sum = ((num / 2) * (num + 1)) + ((num + 1) * (num % 2) / 2);
-	long sumSquared = pow(sum, 2);
+	const long sum = (num * (num + 1) / 2);
+	const long sumSquared = pow(sum, 2);
 	long squaresSum = 0;
 	for (long i = 1; i <= num; i++) {
 		squaresSum += pow(i, 2);
