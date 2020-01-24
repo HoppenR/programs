0e3ef328368b48a379e3283a16e5627dd8d3a830
Christoffer Lundell <christofferlundell@protonmail.com>
2020-01-24 18:11:29 +0100

    Finish up Project Euler solution #8

--- cpp/ProjectEuler/8/main.cpp
+++ cpp/ProjectEuler/8/main.cpp
@@ -1,32 +1,32 @@
 #include <deque>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
-#define CHAROFFSET 48
 
-size_t get_largest_product_n_adj(std::string numbers, size_t nAdjacent) {
+// NOTE: Very easy to run into integer overflows when using 20+ factors
+
+size_t greatest_product_n_adj(std::string numbers, size_t nAdjacent) {
 	size_t currentProduct = 0;
 	size_t greatestProduct = 0;
 	std::deque<int> factors;
-	// Populate the queue before the main loop
 	for (size_t i = 0; i < nAdjacent; i++) {
-		factors.push_back(numbers.at(i) - CHAROFFSET);
+		factors.push_back(numbers.at(i) - '0');
 	}
-	// Check the product of the queue then add next, and remove the first number
 	for (size_t i = nAdjacent; i < numbers.size(); i++) {
 		currentProduct = 1;
 		for (int f : factors) {
 			currentProduct *= static_cast<size_t>(f);
 		}
 		greatestProduct = std::max(greatestProduct, currentProduct);
-		factors.push_back(numbers.at(i) - CHAROFFSET);
+		factors.push_back(numbers.at(i) - '0');
 		factors.pop_front();
 	}
 	return greatestProduct;
 };
 
-int main(void) {
-	std::ifstream file("input");
+std::string file_data(std::string filename) {
+	std::ifstream file(filename);
 	if (file.is_open()) {
 		std::string data;
 		while (file.peek() != EOF) {
@@ -36,6 +36,11 @@ int main(void) {
 			}
 		}
 		file.close();
-		std::cout << get_largest_product_n_adj(data, 13) << '\n';
+		return data;
 	}
+	throw std::runtime_error("File does not exist");
+}
+
+int main(void) {
+	std::cout << greatest_product_n_adj(file_data("numbers.txt"), 13) << '\n';
 }
