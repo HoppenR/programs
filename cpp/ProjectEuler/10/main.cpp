a694a363465b5612a98d00b9d207c8690e4e5832
Christoffer Lundell <christofferlundell@protonmail.com>
2020-01-26 16:51:57 +0100

    Implement Sieve of Eratosthenes in #10

--- cpp/ProjectEuler/10/main.cpp
+++ cpp/ProjectEuler/10/main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 
-// TODO: Optimize this
-
-bool is_prime(const int num) {
-	const int target = sqrt(num);
-	for (int i = 2; i <= target; i++) {
-		if (num % i == 0) {
-			return false;
+long sum_primes_range_n(const size_t upperbound) {
+	// Sums all primes from 2 to upperbound (exclusive)
+	std::vector<bool> numbers(upperbound, true);
+	numbers.at(0) = false;
+	numbers.at(1) = false;
+	const size_t target = sqrt(upperbound);
+	for (size_t i = 0; i <= target; i++) {
+		if (numbers.at(i)) {
+			for (size_t j = pow(i, 2); j < upperbound; j += i) {
+				// Since i is a prime we need to start marking from at least 2i,
+				// but we can start at i² for a slight optimization,
+				// then we mark every multiple of i after our starting point.
+				numbers.at(j) = false;
+			}
 		}
 	}
-	return true;
-}
-
-long sum_primes_range_n(const int upperbound) {
-	long sum = 2;
-	for (int i = 3; i < upperbound; i += 2) {
-		if (is_prime(i))
+	long sum = 0;
+	for (size_t i = 0; i < numbers.size(); i++) {
+		if (numbers.at(i)) {
 			sum += i;
+		}
 	}
 	return sum;
 }
