eae10530231e79a184dfa8f143898aaa90889cb7
Christoffer Lundell <christofferlundell@protonmail.com>
2020-02-13 21:36:19 +0100

    Project Euler #13 switch from gmplib to own implem

--- cpp/ProjectEuler/13/main.cpp
+++ cpp/ProjectEuler/13/main.cpp
@@ -1,27 +1,49 @@
+#include <algorithm>
 #include <fstream>
-#include <gmpxx.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
-// TODO: gmplib is kind of cheating, do this without gmplib
+constexpr int ChrDecDiff = 48; // 1 + 48 = '1'
 
-using MpzVector = std::vector<mpz_class>;
+using StrVec = std::vector<std::string>;
 
-size_t sum_bignum(const MpzVector& numbers, const size_t numdigits) {
-	mpz_class sum;
-	for (const mpz_class& n : numbers) {
-		sum += n;
-	}
-	return std::stoul(sum.get_str().substr(0, numdigits));
+bool max_size_cmp(const std::string& s, const std::string& t) {
+	return s.size() < t.size();
 }
 
-MpzVector file_data_mpz(const std::string& filename) {
+size_t sum_bignum(const StrVec& numbers, const size_t numdigits) {
+	const size_t maxlen =
+		std::max_element(numbers.begin(), numbers.end(), max_size_cmp)->size();
+	std::string sum(maxlen, '0');
+	for (const std::string& n : numbers) {
+		short carry = 0;
+		for (size_t i = 0; i < sum.size(); i++) {
+			short digit = sum.at(sum.size() - 1 - i) - ChrDecDiff + carry;
+			if (i < n.size()) {
+				digit += n.at(n.size() - 1 - i) - ChrDecDiff;
+			}
+			if (digit >= 10) {
+				carry = 1;
+				digit -= 10;
+			} else {
+				carry = 0;
+			}
+			sum.at(sum.size() - 1 - i) = digit + ChrDecDiff;
+		}
+		if (carry) {
+			sum.insert(sum.begin(), carry + ChrDecDiff);
+		}
+	}
+	return std::stoul(sum.substr(0, numdigits));
+}
+
+StrVec file_data_string(const std::string& filename) {
 	std::ifstream ifsnum(filename);
-	MpzVector numbers;
+	StrVec numbers;
 	if (ifsnum.is_open()) {
 		for (std::string line; std::getline(ifsnum, line);) {
-			numbers.push_back(mpz_class(line));
+			numbers.push_back(std::string(line));
 		}
 	} else {
 		throw std::runtime_error("File does not exist");
@@ -30,5 +52,5 @@ MpzVector file_data_mpz(const std::string& filename) {
 }
 
 int main(void) {
-	std::cout << sum_bignum(file_data_mpz("numbers.txt"), 10) << '\n';
+	std::cout << sum_bignum(file_data_string("numbers.txt"), 10) << '\n';
 }
