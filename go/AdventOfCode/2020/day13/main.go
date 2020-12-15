f8b12b10916b773a71788d50f07cafe97087969a
Christoffer Lundell <christofferlundell@protonmail.com>
2020-12-15 13:58:44 +0100

    AoC2020#13: Initial solution part 2

--- go/AdventOfCode/2020/day13/main.go
+++ go/AdventOfCode/2020/day13/main.go
@@ -3,30 +3,50 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
-	"math"
 )
 
 func main() {
-	departure, input, err := ReadInts("input")
+	departure, timetable, err := ReadTimetable("input")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("1:", EarliestBus(departure, input))
+	fmt.Println("1:", EarliestBus(departure, timetable))
+	fmt.Println("2:", DepartureSeriesTime(timetable))
 }
 
-func EarliestBus(departure int, input []interface{}) (ans int) {
+func DepartureSeriesTime(timetable []interface{}) (t int) {
+	busIDs := make([]int, 0)
+	offsets := make([]int, 0)
+	for i, in := range timetable {
+		switch v := in.(type) {
+		case int:
+			busIDs = append(busIDs, v)
+			offsets = append(offsets, i)
+		}
+	}
+	jump := 1
+	for i := range busIDs {
+		for (t+offsets[i])%busIDs[i] != 0 {
+			t += jump
+		}
+		jump *= busIDs[i]
+	}
+	return t
+}
+
+func EarliestBus(departure int, timetable []interface{}) (ans int) {
 	firstBus := math.MaxInt64
 	busID := 0
-	for _, in := range input {
-		switch v:= in.(type) {
+	for _, in := range timetable {
+		switch v := in.(type) {
 		case int:
 			for i := departure; ; i++ {
-				if i % v == 0 {
+				if i%v == 0 {
 					if i < firstBus {
 						firstBus = i
 						busID = v
@@ -39,24 +59,7 @@ func EarliestBus(departure int, input []interface{}) (ans int) {
 	return (firstBus - departure) * busID
 }
 
-func ReadLines(filename string) ([]string, error) {
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	lines := strings.Split(string(content), "\n")
-	return lines, nil
-}
-
-func ReadFile(filename string) (string, error) {
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return "", err
-	}
-	return string(content), nil
-}
-
-func ReadInts(filename string) (int, []interface{}, error) {
+func ReadTimetable(filename string) (int, []interface{}, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return 0, nil, err
