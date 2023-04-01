02eef67cabfaeba4f57cbe1c25562476e7eb0390
Christoffer Lundell <christofferlundell@protonmail.com>
2023-04-01 17:27:58 +0200

    Better editing of entries, reading, json saving

--- rust/grades/src/ui/term.rs
+++ rust/grades/src/ui/term.rs
@@ -1,11 +1,25 @@
 use std::mem;
 
-pub(super) const TERM_BUF_ALT: &str = "\x1b[?1049h";
-pub(super) const TERM_BUF_CLR: &str = "\x1b2J";
-pub(super) const TERM_BUF_PRI: &str = "\x1b[?1049l";
-pub(super) const TERM_CURS_HOME: &str = "\x1b[H";
-pub(super) const TERM_CURS_INVIS: &str = "\x1b[?25l";
-pub(super) const TERM_CURS_VIS: &str = "\x1b[?25h";
+pub(crate) const RST: &str = "\x1b[0m"; // Reset
+pub(crate) const BLD: &str = "\x1b[1m"; // Bold
+pub(crate) const CUR: &str = "\x1b[3m"; // Cursive
+pub(crate) const UDL: &str = "\x1b[4m"; // Underline
+pub(crate) const STK: &str = "\x1b[9m"; // Strikethrough
+pub(crate) const RED: &str = "\x1b[91m"; // Red
+pub(crate) const GRN: &str = "\x1b[92m"; // Green
+pub(crate) const YLW: &str = "\x1b[93m"; // Yellow
+pub(crate) const BLU: &str = "\x1b[94m"; // Blue
+pub(crate) const CYN: &str = "\x1b[96m"; // Cyan
+
+pub(crate) const ERASE_TO_LINE_END: &str = "\x1b[0K";
+pub(crate) const ERASE_TO_DISP_END: &str = "\x1b[J";
+
+pub(super) const BUF_ALT: &str = "\x1b[?1049h";
+pub(super) const BUF_CLR: &str = "\x1b2J";
+pub(super) const BUF_PRI: &str = "\x1b[?1049l";
+pub(super) const CURS_HOME: &str = "\x1b[H";
+pub(super) const CURS_INVIS: &str = "\x1b[?25l";
+pub(super) const CURS_VIS: &str = "\x1b[?25h";
 
 pub(super) unsafe fn set_noraw_terminal_mode(old_termios: &mut libc::termios) {
     libc::tcsetattr(libc::STDOUT_FILENO, libc::TCSANOW, old_termios);
--- rust/grades/src/uni_info.rs
+++ rust/grades/src/uni_info.rs
@@ -1,15 +1,36 @@
 mod cursor;
 mod print;
 
-use cursor::{Cursor, CursorLevel};
-use serde::Deserialize;
-use std::borrow::BorrowMut;
-use std::cmp;
+pub(super) use cursor::*;
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+use std::ops::BitXorAssign;
 
-type Tasks = BTreeMap<String, bool>;
-
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Serialize)]
+pub(super) struct UniInfo {
+    menu: Menu,
+    #[serde(default, skip_serializing)]
+    pub(super) cursor: Cursor,
+}
+type Menu = Vec<Semester>;
+type Semester = [Period; 2];
+type Period = Vec<Course>;
+#[derive(Deserialize, Serialize, Clone)]
+struct Course {
+    code: String,
+    grade: Grade,
+    moments: Moments,
+    name: String,
+}
+#[derive(Deserialize, Serialize, PartialEq, Clone)]
+#[serde(untagged)]
+pub(super) enum Grade {
+    Completed(bool),
+    Grade(usize),
+    Ongoing,
+}
+type Moments = Vec<Moment>;
+#[derive(Deserialize, Serialize, Clone)]
 struct Moment {
     code: String,
     completed: bool,
@@ -17,54 +38,11 @@ struct Moment {
     description: String,
     tasks: Option<Tasks>,
 }
-
-type Moments = Vec<Moment>;
-
-#[derive(Deserialize, PartialEq, Clone)]
-#[serde(untagged)]
-enum Grade {
-    Completed(bool),
-    Grade(usize),
-    Ongoing,
-}
-
-#[derive(Deserialize, Clone)]
-struct Course {
-    code: String,
-    grade: Grade,
-    moments: Moments,
+type Tasks = BTreeMap<String, bool>;
+// Helper struct to implement Display for a tuple when iterating over `Tasks`
+struct PrintableTask {
     name: String,
-}
-
-type Period = Vec<Course>;
-
-#[derive(Deserialize, Clone)]
-struct Semester {
-    index: usize,
-    periods: [Period; 2],
-}
-
-type Menu = Vec<Semester>;
-
-#[derive(Deserialize)]
-pub(super) struct UniInfo {
-    menu: Menu,
-    #[serde(default)]
-    cursor: Cursor,
-}
-
-impl Course {
-    fn sum_credits(&self) -> f32 {
-        return self
-            .moments
-            .iter()
-            .filter_map(|v| if v.completed { Some(v.credits) } else { None })
-            .sum();
-    }
-
-    fn max_credits(&self) -> f32 {
-        return self.moments.iter().map(|v| v.credits).sum();
-    }
+    completed: bool,
 }
 
 impl UniInfo {
@@ -87,8 +65,8 @@ impl UniInfo {
         let num_entries_next_level: usize = match self.cursor.level {
             CursorLevel::Semester => self.sel_semester_entries(),
             CursorLevel::Period => self.sel_period_entries(),
-            CursorLevel::Course => match self.sel_course().map(|x| x.grade == Grade::Ongoing) {
-                Some(ongoing) if ongoing => self.sel_course_entries(),
+            CursorLevel::Course => match self.sel_course().map(|x| x.should_print_moments()) {
+                Some(true) => self.sel_course_entries(),
                 _ => 0,
             },
             CursorLevel::Moment => self.sel_moment_entries(),
@@ -106,29 +84,26 @@ impl UniInfo {
         }
     }
 
-    pub(super) fn edit_selection(&mut self) {
-        match self.cursor.level {
-            CursorLevel::Semester => {}
-            CursorLevel::Period => {}
-            CursorLevel::Course => {
-                if let Some(course) = self.sel_course_mut() {
-                    match course.grade.borrow_mut() {
-                        Grade::Completed(completed) => *completed = !*completed,
-                        Grade::Grade(grade) => *grade = cmp::max(3, (*grade + 1) % 6),
-                        Grade::Ongoing => {}
-                    }
-                }
-            }
-            CursorLevel::Moment => {
-                if let Some(moment) = self.sel_moment_mut() {
-                    moment.completed = !moment.completed;
-                }
-            }
-            CursorLevel::Task => {
-                if let Some((_, completed)) = self.sel_task_mut() {
-                    *completed = !*completed;
-                }
-            }
+    pub(super) fn set_selected_course(&mut self, new_grade: Grade) {
+        if let Some(course) = self.sel_course_mut() {
+            course.grade = new_grade;
+        }
+    }
+
+    pub(super) fn toggle_selected_moment(&mut self) {
+        if let Some(moment) = self.sel_moment_mut() {
+            moment.completed.bitxor_assign(true);
+            // if let Some(tasks) = moment.tasks.as_mut() {
+            //     for (_, completion) in tasks {
+            //         *completion = moment.completed;
+            //     }
+            // }
+        }
+    }
+
+    pub(super) fn toggle_selected_task(&mut self) {
+        if let Some((_, completed)) = self.sel_task_mut() {
+            completed.bitxor_assign(true);
         }
     }
 
@@ -141,7 +116,7 @@ impl UniInfo {
 
     fn sel_semester_entries(&self) -> usize {
         match self.sel_semester() {
-            Some(semester) => semester.periods.len(),
+            Some(semester) => semester.len(),
             _ => 0,
         }
     }
@@ -161,7 +136,7 @@ impl UniInfo {
     }
 
     fn sel_moment_entries(&self) -> usize {
-        match self.sel_moment().and_then(|x| x.tasks.clone()) {
+        match self.sel_moment().and_then(|x| x.tasks.as_ref()) {
             Some(tasks) => tasks.len(),
             _ => 0,
         }
@@ -172,27 +147,27 @@ impl UniInfo {
     }
 
     fn sel_semester(&self) -> Option<&Semester> {
-        self.sel_menu().and_then(|x| x.get(self.cursor.semester_ix))
+        self.sel_menu()?.get(self.cursor.semester_ix)
     }
 
     fn sel_period(&self) -> Option<&Period> {
-        self.sel_semester()
-            .map(|v| &v.periods[self.cursor.period_ix])
+        self.sel_semester()?.get(self.cursor.period_ix)
     }
 
     fn sel_course(&self) -> Option<&Course> {
-        self.sel_period().and_then(|x| x.get(self.cursor.course_ix))
+        self.sel_period()?.get(self.cursor.course_ix)
     }
 
     fn sel_moment(&self) -> Option<&Moment> {
-        self.sel_course()
-            .and_then(|x| x.moments.get(self.cursor.moment_ix))
+        self.sel_course()?.moments.get(self.cursor.moment_ix)
     }
 
     // fn sel_task(&self) -> Option<(&String, &bool)> {
-    //     self.sel_moment()
-    //         .and_then(|x| x.tasks.as_ref())
-    //         .and_then(|x| x.iter().nth(self.cursor.task_ix))
+    //     self.sel_moment()?
+    //         .tasks
+    //         .as_ref()?
+    //         .iter()
+    //         .nth(self.cursor.task_ix)
     // }
 
     fn sel_menu_mut(&mut self) -> Option<&mut Menu> {
@@ -200,32 +175,58 @@ impl UniInfo {
     }
 
     fn sel_semester_mut(&mut self) -> Option<&mut Semester> {
-        let ix = self.cursor.semester_ix;
+        let ix: usize = self.cursor.semester_ix;
         self.sel_menu_mut()?.get_mut(ix)
     }
 
     fn sel_period_mut(&mut self) -> Option<&mut Period> {
-        let ix = self.cursor.period_ix;
-        self.sel_semester_mut()?.periods.iter_mut().nth(ix)
+        let ix: usize = self.cursor.period_ix;
+        self.sel_semester_mut()?.get_mut(ix)
     }
 
     fn sel_course_mut(&mut self) -> Option<&mut Course> {
-        let ix = self.cursor.course_ix;
+        let ix: usize = self.cursor.course_ix;
         self.sel_period_mut()?.get_mut(ix)
     }
 
     fn sel_moment_mut(&mut self) -> Option<&mut Moment> {
-        let ix = self.cursor.moment_ix;
+        let ix: usize = self.cursor.moment_ix;
         self.sel_course_mut()?.moments.get_mut(ix)
     }
 
     fn sel_task_mut(&mut self) -> Option<(&String, &mut bool)> {
-        let ix = self.cursor.task_ix;
-        if let Some(moment) = self.sel_moment_mut() {
-            if let Some(tasks) = &mut moment.tasks {
-                return tasks.iter_mut().nth(ix);
-            }
+        let ix: usize = self.cursor.task_ix;
+        self.sel_moment_mut()?.tasks.as_mut()?.iter_mut().nth(ix)
+    }
+}
+
+impl Course {
+    fn sum_credits(&self) -> f32 {
+        return self
+            .moments
+            .iter()
+            .filter_map(|v| if v.completed { Some(v.credits) } else { None })
+            .sum();
+    }
+
+    fn max_credits(&self) -> f32 {
+        return self.moments.iter().map(|v| v.credits).sum();
+    }
+
+    fn should_print_moments(&self) -> bool {
+        match self.grade {
+            Grade::Ongoing => true,
+            Grade::Completed(passed) => !passed,
+            Grade::Grade(_) => false,
         }
-        None
+    }
+}
+
+impl From<(&String, &bool)> for PrintableTask {
+    fn from(value: (&String, &bool)) -> Self {
+        PrintableTask {
+            name: value.0.clone(),
+            completed: *value.1,
+        }
     }
 }
