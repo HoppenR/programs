8d1e3daf0a5dbc02e0a4215b0d6f7f62a98052c0
Christoffer Lundell <christofferlundell@protonmail.com>
2023-04-03 00:49:01 +0200

    key: own StdinLock,only use new bytes, compr-macro

--- rust/grades/src/ui/key.rs
+++ rust/grades/src/ui/key.rs
@@ -18,23 +18,60 @@
 //! }
 //! ```
 
-use std::io::{self, Error, Read};
+use std::io::{self, Error, Read, StdinLock};
 use std::str;
 
-/// `Key` represents data from a keypress.
-pub(super) struct Key {
-    data: [u8; 6],
+const ESC: u8 = 27;
+
+/// Maps key names to patterns to match common terminal representations.
+/// The terminal representations will differ between terminals.
+/// For example `alacritty` and `xterm` will send `0x7f` and `0x08`
+/// respectively when pressing backspace.
+macro_rules! keycode {
+    ("backspace") => {
+        [8] | [127]
+    };
+    ("down") => {
+        [ESC, b'[', b'B']
+    };
+    ("enter") => {
+        [b'\r']
+    };
+    ("esc") => {
+        [ESC]
+    };
+    ("left") => {
+        [ESC, b'[', b'D']
+    };
+    ("right") => {
+        [ESC, b'[', b'C']
+    };
+    ("up") => {
+        [ESC, b'[', b'A']
+    };
 }
 
-impl Key {
+/// `Key` represents data from a keypress.
+pub(super) struct Key<'a> {
+    data: [u8; 6],
+    is: StdinLock<'a>,
+    rd: usize,
+}
+
+impl<'a> Key<'a> {
     pub(super) fn new() -> Self {
-        Key { data: [0; 6] }
+        let is: StdinLock = io::stdin().lock();
+        Key {
+            data: [0; 6],
+            is,
+            rd: 0,
+        }
     }
 
     /// Blocking read a keypress from standard input.
     pub(super) fn read(&mut self) -> Result<(), Error> {
-        let bytes_read: usize = io::stdin().read(&mut self.data[..])?;
-        if bytes_read == 0 {
+        self.rd = self.is.read(&mut self.data[..])?;
+        if self.rd == 0 {
             return Err(Error::new(
                 io::ErrorKind::InvalidData,
                 "invalid input, 0 bytes read",
@@ -46,8 +83,10 @@ impl Key {
     /// Returns an optional character if the last keypress is representable
     /// as a printable ascii-character.
     pub(super) fn as_printable_ascii(&self) -> Option<char> {
-        if matches!(self.data, [b' '..=b'~', ..]) {
-            return Some(self.data[0] as char);
+        if self.rd == 1 {
+            if matches!(self.data[0], b' '..=b'~') {
+                return Some(self.data[0] as char);
+            }
         }
         None
     }
@@ -55,7 +94,7 @@ impl Key {
     /// Returns an optional character if the last keypress is representable
     /// as a printable utf8-character.
     pub(super) fn as_printable_utf8(&self) -> Option<char> {
-        match str::from_utf8(&self.data[..]) {
+        match str::from_utf8(&self.data[..self.rd]) {
             Ok(data_str) => match data_str.chars().next() {
                 Some('\u{00}'..='\u{1f}') => None,
                 Some('\u{7f}'..='\u{9f}') => None,
@@ -71,37 +110,36 @@ impl Key {
     }
 
     /// Checks whether the last keypress is the backspace or delete character.
-    /// `alacritty` and `xterm` will send `0x7f` and `0x08` respectively for this.
     pub(super) fn is_backspace(&self) -> bool {
-        matches!(self.data, [8, ..] | [127, ..])
+        matches!(self.data[..self.rd], keycode!("backspace"))
     }
 
     /// Checks whether the last keypress is the enter (return) key.
     pub(super) fn is_enter(&self) -> bool {
-        matches!(self.data, [b'\r', ..])
+        matches!(self.data[..self.rd], keycode!("enter"))
     }
 
     pub(super) fn is_esc(&self) -> bool {
-        matches!(self.data, [27, b'\0', ..])
+        matches!(self.data[..self.rd], keycode!("esc"))
     }
 
     /// Checks whether the last keypress is the up-arrow.
     pub(super) fn is_up(&self) -> bool {
-        matches!(self.data, [27, b'[', b'A', ..])
+        matches!(self.data[..self.rd], keycode!("up"))
     }
 
     /// Checks whether the last keypress is the down-arrow.
     pub(super) fn is_down(&self) -> bool {
-        matches!(self.data, [27, b'[', b'B', ..])
+        matches!(self.data[..self.rd], keycode!("down"))
     }
 
     /// Checks whether the last keypress is the right-arrow.
     pub(super) fn is_right(&self) -> bool {
-        matches!(self.data, [27, b'[', b'C', ..])
+        matches!(self.data[..self.rd], keycode!("right"))
     }
 
     /// Checks whether the last keypress is the left-arrow.
     pub(super) fn is_left(&self) -> bool {
-        matches!(self.data, [27, b'[', b'D', ..])
+        matches!(self.data[..self.rd], keycode!("left"))
     }
 }
